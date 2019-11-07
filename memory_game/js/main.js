6c32df22a9127abd353abc88128b2edb166398c1
Heather Rocco <heather@Heathers-MacBook-Pro.local>
2019-11-07 15:17:09 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,37 @@
-console.log("Up and running!")
 
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
 
-console.log("user flipped " + cardOne);
-console.log("user flipper " + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+
+
+console.log("User flipped " + cardOne);
+
+cardsInPlay.push(cardOne);
+
+console.log(cardsInPlay);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardTwo);
+
+console.log(cardsInPlay);
+
+
+if (cardsInPlay.length === 2){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+
+}
+
+
+
